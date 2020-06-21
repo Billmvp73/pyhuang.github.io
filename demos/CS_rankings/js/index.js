f8729c6225d9b873fab2efc948d3d9c51bea1739
pyhuang <pyhuang@umich.edu>
2020-06-21 13:29:04 -0400

    update simplified cs_ranking

--- demos/CS_rankings/js/index.js
+++ demos/CS_rankings/js/index.js
@@ -1,23 +1,6 @@
-var VueComponent1 = Vue.extend({
-    template: '#app1',
-    data: function() {
-        return {
-            Directions: []
-        }
-    }
-})
-
-var VueComponent2=Vue.extend({
-    template: '#app2',
-    data: function() {
-        return {
-            output: []
-        }
-    }
-})
 var vm = new Vue(
     {
-        el: 'body',
+        el: '#app',
         data:{
             checkedFields:[],
             Directions :[
@@ -47,21 +30,7 @@ var vm = new Vue(
         methods: {
             check: function(e){
                 console.log(this.checkedFields);
-                fetch('./rankings.json')
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .then((data) => {
-                        var i = 0;
-                        for(i=0; i < this.checkedFields.length;i++){
-                            var field = this.checkedFields[i];
-                            this.output.push(data.field);
-                        }
-                    })
             }
         }
     }
 )
-
-new VueComponent1().$mount('#app1');
-var app2 = Vue.component('app2', VueComponent2);
